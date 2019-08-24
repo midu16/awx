ee6d701f93da02d470a5deaed019c201ecfac543
midu <mihai.idu@canonical.com>
2019-08-24 20:18:07 +0200

    update

---
+++ aws.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# changing the directory to the project one
+dir=$(pwd)
+cd dir/aws
+
+docker-compose up -d
+
+#connection
+# check the http://localhost/
+# user: admin
+# pass: password
+
